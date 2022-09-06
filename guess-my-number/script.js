8bd21790ce686b200fc3871fc75c19fab42c18cf
Lee <hldodson@gmail.com>
2022-09-06 20:32:10 +0000

    changed some css when the game is won.

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -12,23 +12,25 @@ const lostGameMessage = 'You lost the game!';
 console.log(ans);
 let score = 20;
 
-document.querySelector('.check').addEventListener('click', function () {
+document.querySelector('.check').addEventListener('click', function () { // note for querySelector, dot for class hash for id.
     const guess = Number(document.querySelector('.guess').value);
     if (!guess) {
-        document.querySelector('.message').textContent = 'Please choose a value!'
-    } else if (guess === ans) {
-        document.querySelector('.message').textContent = 'Correct Answer!'
-    } else if (guess > ans) {
+        document.querySelector('.message').textContent = 'Please choose a value!';
+    } else if (guess === ans) { //when the guess is correct
+        document.querySelector('.message').textContent = 'Correct Answer!';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+    } else if (guess > ans) { // when the guess is too high
         if (score > 1) {
-            document.querySelector('.message').textContent = 'Too High!'
+            document.querySelector('.message').textContent = 'Too High!';
             document.querySelector('.score').textContent = --score;
         } else {
             document.querySelector('.message').textContent = lostGameMessage;
             document.querySelector('.score').textContent = 0;
         }
-    } else if (guess < ans) {
+    } else if (guess < ans) { // when the guess is too low
         if (score > 1) {
-            document.querySelector('.message').textContent = 'Too Low!'
+            document.querySelector('.message').textContent = 'Too Low!';
             document.querySelector('.score').textContent = --score;
         } else {
             document.querySelector('.message').textContent = lostGameMessage;
